747cf7e833ba588bcec4ab429d72fd3767b0cdbc
ZhongZhaorui <1351499903@qq.com>
2018-06-02 01:56:07 +0800

    修复括号运算的bug

--- calculator/2.2.js
+++ calculator/2.2.js
@@ -4,8 +4,8 @@ var rm=document.getElementById('btn1');
 var em=document.getElementById('btn2');
 var eq=document.getElementById('eq');
 var num=document.getElementsByClassName('num');
-        var sn=[];
-		var arr="";
+    var sn=[];//存全部的
+		var arr="";//过渡
 		var number=[];//存数字
 		var symbol=[];//存符号
 for (var i = 0; i < num.length; i++) {
@@ -13,128 +13,130 @@ for (var i = 0; i < num.length; i++) {
 		arr=arr.concat("",this.innerText);
 		console.log(arr);
 		output.innerText+=this.innerText;
-		if(this.innerText=="-"||this.innerText=="+"||this.innerText=="*"||this.innerText=="/"||this.innerText=="="){
-		var index=arr.indexOf(this.innerText);
-        var number1=arr.slice(0, index);
+		if(this.innerText=="-"||this.innerText=="+"||this.innerText=="*"||this.innerText=="/"||this.innerText=="="||this.innerText=="("||this.innerText==")"){
+		var index=arr.indexOf(this.innerText);//获取当前符号的索引
+        var number1=arr.slice(0, index);//获取当前符号前的数字（字符串）
         number.push(number1);
         symbol.push(this.innerText);
         var slen=symbol.length-1;//除开等号的长度
         arr="";
 		}
 
-		console.log(number);
+		    console.log(number);
         console.log(symbol);
         for (var j = 0; j < symbol.length; j++) {
         	sn[j*2]=number[j];
         	sn[j*2+1]=symbol[j];
         }
+         Array.prototype.removeEmptyEle = function(arr){
+    for(var i = 0; i < arr.length; i++) {
+      if(arr[i] =="") {
+         arr.splice(i,1);
+         i = i - 1; // i - 1 ,因为空元素在数组下标 2 位置，删除空之后，后面的元素要向前补位，
+                       // 这样才能真正去掉空元素,觉得这句可以删掉的连续为空试试，然后思考其中逻辑
+       }
+    }
+      return arr;
+   };
+   sn=sn.removeEmptyEle(sn);
         console.log(sn);var lastresult=0;var tplen;
-        
-
-
-        function mix(){
-
-
-        }
-        for (var i = 0; i < symbol.length; i++) {
-            if ((symbol.indexOf("*")<0)&&(symbol.indexOf("/")<0)) {
-            add();
-               if(symbol[symbol.length-1]=="="){ 
+        // 纯加减
+        // for (var i = 0; i < symbol.length; i++) {
+        //     if ((symbol.indexOf("*")<0)&&(symbol.indexOf("/")<0)) {
+        //     add();
+        //        if(symbol[symbol.length-1]=="="){ 
     
-            result.innerText=number[0];
-        }
-        }
-            if ((symbol.indexOf("+")<0)&&(symbol.indexOf("-")<0)) {
-            mul();
-               if(symbol[symbol.length-1]=="="){ 
+        //     result.innerText=number[0];
+        // }
+        // }
+        // //纯乘除
+        //     if ((symbol.indexOf("+")<0)&&(symbol.indexOf("-")<0)) {
+        //     mul();
+        //        if(symbol[symbol.length-1]=="="){ 
     
-            result.innerText=number[0];
-        }
+        //     result.innerText=number[0];
+        // }
 
-        }
-        
-         
-        }
-      
-           if (((symbol.indexOf("*")>=0)||(symbol.indexOf("/")>=0))&&(symbol.indexOf("+")>=0)||(symbol.indexOf("-")>=0)) {
-               sn=sn.slice(0, sn.length-1);
-               midTOLast(sn);
+        // }   
+        // }
+      //混合计算
+           // if (((symbol.indexOf("*")>=0)||(symbol.indexOf("/")>=0))&&(symbol.indexOf("+")>=0)||(symbol.indexOf("-")>=0)) {
+               sn=sn.slice(0, sn.length-1);//去除等号（“=”）
+               midTOLast(sn);//调用中缀转后缀函数
                console.log(b);
-               var value=getValue(b);
-                   if(symbol[symbol.length-1]=="="){ 
+               var value=getValue(b);//调用后缀求值函数
+              if(symbol[symbol.length-1]=="="){ 
     
-            result.innerText=value;
-        }
+                          result.innerText=value;
+                      }
 
-            }
-     
-
-
-
+            // }
 
 	}
 }
-	function add(){
-            for (var i = 0; i < symbol.length-1; i++) {
-                if (symbol[i]=="+") {
-                    var tp=Number(number[i])+Number(number[i+1]);
-                    number.splice(i,2,tp);
-                    symbol.splice(i,1);
-                }
-                else{
-                    var tp=Number(number[i])-Number(number[i+1]);
-                    number.splice(i,2,tp);
-                    symbol.splice(i,1);
-                    }
+//纯加减函数
+	// function add(){
+ //            for (var i = 0; i < symbol.length-1; i++) {
+ //                if (symbol[i]=="+") {
+ //                    var tp=Number(number[i])+Number(number[i+1]);
+ //                    number.splice(i,2,tp);
+ //                    symbol.splice(i,1);
+ //                }
+ //                else{
+ //                    var tp=Number(number[i])-Number(number[i+1]);
+ //                    number.splice(i,2,tp);
+ //                    symbol.splice(i,1);
+ //                    }
                
-            }
-            return number;
+ //            }
+ //            return number;
 
-        }
-        function mul(){
-             for (var i = 0; i < symbol.length-1; i++) {
-                if (symbol[i]=="*") {
-                    var tp=Number(number[i])*Number(number[i+1]);
-                    number.splice(i,2,tp);
-                    symbol.splice(i,1);
-                }
-                else{
-                    var tp=Number(number[i])/Number(number[i+1]);
-                    number.splice(i,2,tp);
-                    symbol.splice(i,1);
-                    }
+ //        }
+ //        //纯乘除函数
+ //        function mul(){
+ //             for (var i = 0; i < symbol.length-1; i++) {
+ //                if (symbol[i]=="*") {
+ //                    var tp=Number(number[i])*Number(number[i+1]);
+ //                    number.splice(i,2,tp);
+ //                    symbol.splice(i,1);
+ //                }
+ //                else{
+ //                    var tp=Number(number[i])/Number(number[i+1]);
+ //                    number.splice(i,2,tp);
+ //                    symbol.splice(i,1);
+ //                    }
                 
-            }
-            return number;
+ //            }
+ //            return number;
 
-        }
-        function find(a,x){
-          var results=[],
-            len=a.length,
-            pos=0;
-          while(pos<len){
-           pos=a.indexOf(x,pos);
-           if(pos===-1){//未找到就退出循环完成搜索
-            break;
-           }
-           results.push(pos);//找到就存储索引
-           pos+=1;//并从下个位置开始搜索
-          }
-          return results;
-         }
+ //        }
+        // function find(a,x){
+        //   var results=[],
+        //     len=a.length,
+        //     pos=0;
+        //   while(pos<len){
+        //    pos=a.indexOf(x,pos);
+        //    if(pos===-1){//未找到就退出循环完成搜索
+        //     break;
+        //    }
+        //    results.push(pos);//找到就存储索引
+        //    pos+=1;//并从下个位置开始搜索
+        //   }
+        //   return results;
+        //  }
          
-         console.log(find(symbol,"*"));
+        //  console.log(find(symbol,"*"));
         
-         console.log(find(symbol,"/"));
+        //  console.log(find(symbol,"/"));
 
 
 
 
-
+//中缀表达式转后缀表达式函数
  function midTOLast(a){
   var a_len=a.length;
-  var myArray=new Array();
-  b=[];
+  var myArray=new Array();//作为存后缀表达式的过渡数组
+  b=[];//存后缀表达式的数组
   for(var i=0;i<a_len;i++){
    switch (a[i]){
     case '(':
@@ -153,21 +155,11 @@ for (var i = 0; i < num.length; i++) {
       if(myArray.length==0){//保证栈为空
        break;
       }
-      temp=myArray.pop();
+      temp=myArray.pop();//弹出“（”
      }
      break;
     }
-    case '*':{
-     if(myArray.length==0){//如果栈为空则直接入栈
-      myArray.push(a[i]);
-     }else{
-      temp=myArray[myArray.length-1];
-      if(temp=='+'||temp=='-'){//如果遇到高的，则遇到的继续入栈
-       myArray.push(a[i]);//遇到的入栈
-      }
-     }
-     break;
-    }
+    case '*':
     case '/':
     {
      if(myArray.length==0){//如果栈为空则直接入栈
@@ -177,41 +169,33 @@ for (var i = 0; i < num.length; i++) {
       if(temp=='+'||temp=='-'){//如果遇到高的，则遇到的继续入栈
        myArray.push(a[i]);//遇到的入栈
       }
-     }
-     break;
-    }
-    case '+':{
-     if(myArray.length==0){//如果栈为空则直接入栈
-      myArray.push(a[i]);
-     }else{
-      temp=myArray[myArray.length-1];
-      if(temp=='/'||temp=='*'){//如果遇到低的，则栈中的出栈，遇到的入栈
-       while(myArray.length!=0){
-        temp=myArray.pop();//栈中的出栈
-        b.push(temp);//保存到存储空间
-       }
-       myArray.push(a[i]);//遇到的入栈
+        else{
+        myArray.push(a[i]);
       }
      }
      break;
     }
+    case '+': 
     case '-':
     {
      if(myArray.length==0){//如果栈为空则直接入栈
       myArray.push(a[i]);
      }else{
       temp=myArray[myArray.length-1];
-      if(temp=='/'||temp=='*'){//如果遇到低的，则栈中的出栈，遇到的入栈
+      if(temp=='/'||temp=='*'||temp=='-'||temp=='+'){//如果遇到低的，则栈中的出栈，遇到的入栈
        while(myArray.length!=0){
         temp=myArray.pop();//栈中的出栈
         b.push(temp);//保存到存储空间
        }
        myArray.push(a[i]);//遇到的入栈
       }
+      else{
+        myArray.push(a[i]);//遇到的入栈
+      }
      }
      break;
     }
-    default:
+    default://将数字进栈
     {
      b.push(a[i]);
      break;
@@ -230,22 +214,7 @@ for (var i = 0; i < num.length; i++) {
    myArray=new Array();
    for(var i=0;i<a_len;i++){
     switch (a[i])
-    {//遇到数值则直接入栈
-    //  case '0':
-    //  case '1':
-    //  case '2':
-    //  case '3':
-    //  case '4':
-    //  case '5':
-    //  case '6':
-    //  case '7':
-    //  case '8':
-    //  case '9':
-    //  case "10":
-    //  {
-    //   myArray.push(a[i]);
-    //   break;
-    //  }
+    {     
      case '+':
      {//遇到操作符则出栈两个元素进行对应操作
       temp=Number(myArray.pop())+Number(myArray.pop());
@@ -276,6 +245,7 @@ for (var i = 0; i < num.length; i++) {
       s=null;temp=null;
       break;
      }
+     //遇到数值则直接入栈
      default:{
 
       myArray.push(a[i]);
@@ -297,7 +267,7 @@ for (var i = 0; i < num.length; i++) {
 	// 	X的作用
 	rm.onclick=function(){
 	var str=output.innerText;
-	arr=arr.slice(0, arr.length-1);
+	arr=arr.slice(0, arr.length-1);//删除过渡的数组最后一项
 	len=str.length-1;
-	output.innerText=str.slice(0, len);		
+	output.innerText=str.slice(0, len);	//删除输出的最后一项	
 	}
